f1784786aa3fa65b1a61ee9e9868b497dac6b3e9
Rich <114684848+EugeneBelford1995@users.noreply.github.com>
2023-12-30 17:45:26 -0500

    Add files via upload
    Just import this one and then 'Start-Lab' to start the lab & RDP in or 'Stop-Lab' to stop all VMs.

---
+++ Start & Stop all VMs.ps1
@@ -0,0 +1,19 @@
+﻿Function Start-Lab
+{
+Connect-AzAccount
+
+Start-AzVM -ResourceGroupName ADLab -Name DC1
+Start-AzVM -ResourceGroupName ADLab -Name DC2
+Start-AzVM -ResourceGroupName ADLab -Name MemberServer
+
+Write-Host "Login with username ADLabAdmin@ADLab.local \ password MySuperSecurePassword00!!"
+$PIP = Get-AzPublicIpAddress
+mstsc /v: $PIP.IpAddress 
+}
+
+Function Stop-Lab
+{
+Stop-AzVM -ResourceGroupName ADLab -Name DC1 -Confirm
+Stop-AzVM -ResourceGroupName ADLab -Name DC2 -Confirm
+Stop-AzVM -ResourceGroupName ADLab -Name MemberServer -Confirm
+}
